70dfb333a1a1978872a756b5a11566b6ad2efa57
jujusb <jsantilariob@gmail.com>
2026-06-30 16:34:53 +0000

    Update subproject commits and enhance Docker configuration for Caddy service

--- run-multiservers.sh
+++ run-multiservers.sh
@@ -7,6 +7,7 @@ ensure_network() {
         docker network create "$net"
     fi
 }
+
 ensure_network caddy_net
 source .env
 echo $SERVICES
@@ -21,6 +22,16 @@ for SERVICE in "${SERVICES_ARRAY[@]}"; do
 done
 
 source .env
+is_ip() {
+  [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]
+}
+if is_ip "$SERVER_ADDRESS"; then
+    echo "Detected IP → using internal TLS"
+    export TLS_CONFIG='tls internal'
+else
+    echo "Detected domain → using Cloudflare TLS"
+    export TLS_CONFIG='tls { dns cloudflare {env.CF_API_TOKEN} }'
+fi
 export COMPOSE_FILE="docker-compose.yml"
 docker compose $@
 unset COMPOSE_FILE
